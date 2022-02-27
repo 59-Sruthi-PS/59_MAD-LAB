0eb350354ae4fbb5b0bbe9fb5d87ae97c7e345ac
Sruthi PS <91335951+59-Sruthi-PS@users.noreply.github.com>
2022-02-27 23:32:51 +0530

    toggle

---
+++ Toggle_Button/src/main/java/com/example/toggle_button/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.toggle_button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    ToggleButton toggleButton;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toggleButton=findViewById(R.id.toggleButton);
+        imageView=findViewById(R.id.imageView);
+
+        imageView.setImageDrawable(getResources().getDrawable(R.drawable.noti_off));
+
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(toggleButton.isChecked())
+                {
+                    imageView.setImageDrawable(getResources().getDrawable(R.drawable.noti_on));
+                }else
+                {
+                    imageView.setImageDrawable(getResources().getDrawable(R.drawable.noti_off));
+                }
+            }
+        });
+    }
+}
